7327017eee9ad41676a9efed00fba91d154abd0b
SoumailaBahngobi <bahngobisoumaila@gmail.com>
2024-12-05 01:05:22 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -58,10 +58,10 @@ function App() {
         <p className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">FIND YOUR STYLE</p>
 
       </div>
-      {/* <Outdoors /> */}
-      {/* <Contact /> */}
-      {/* <Kitchen /> */}
-      {/* < AboutUs /> */}
+      <Outdoors />
+      <Contact />
+      <Kitchen />
+      < AboutUs />
       <AboutUs />
       <Footer />
       <br />
